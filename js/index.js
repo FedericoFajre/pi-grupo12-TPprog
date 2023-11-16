7fc892d8ce7c6a7c313d430dd3369a5765696660
Matelavan <lavandeiram@udesa.edu.ar>
2023-11-16 16:28:56 -0300

    navegador y resultados de busqueda

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+let form = document.querySelector("form");
+let input = document.querySelector("input");
+            
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+            
+    if(input.value === ""){
+        alert ("Tenes que completar el campo!")
+                }  else if (input.value.length < 3){
+      alert ("Debe introducir minimo 3 caracteres");
+            } else {
+        this.submit()
+    }
+})
+    
 let acaVaLaAPIKey = "ba751b79f4d33c473bf6d1b115cc817d";
 
 let urlP = `https://api.themoviedb.org/3/movie/popular?api_key=${acaVaLaAPIKey}`;
