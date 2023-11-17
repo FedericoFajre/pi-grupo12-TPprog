ddfea768e9ae848507cbb738c5c9002d5cc4bcb6
Matelavan <lavandeiram@udesa.edu.ar>
2023-11-17 15:07:33 -0300

    casi terminada la busqueda

--- js/busqueda.js
+++ js/busqueda.js
@@ -16,18 +16,34 @@ form.addEventListener("submit", function(e){
 /* Resultados de busqueda */
 let acaVaLaAPIKey = "ba751b79f4d33c473bf6d1b115cc817d";
 
-let qs = location.search
-let qsObjLit = new URLSearchParams(qs);
-let buscar = qsObjLit.get('Buscador');
-console.log(buscar);
+let querystring = location.search
+let qsToObject = new URLSearchParams(querystring)
+let datoAb = qsToObject.get("buscar")
 
+let busqueda = document.querySelector(".titulos")
+busqueda.innerHTML += `<h2>Search results for <i>${datoAb}</i></h2>`
 
-let url_searchPelis = `https://api.themoviedb.org/3/search/movie/${buscar}?api_key=${acaVaLaAPIKey}`;
-fetch(url_searchPelis)
+let titulos = document.querySelector (".titulos")
+let pelis = document.querySelector (".PadrePeli")
+let imagen = document.querySelector (".tamaño")
+
+let url = `https://api.themoviedb.org/3/search/movie?query=${datoAb}&${acaVaLaAPIKey}&include_adult=false&language=en-US&page=1`;
+
+fetch(url)
     .then(function(response){
         return response.json()
     })
     .then(function(data){
         console.log(data);
         
-       
+        let seccionBuscador= document.querySelector(".oculto");
+        let mensaje = document.querySelector(".mensaje")
+
+        if(data.results.length == 0){
+            mensaje.innerHTML = `No se ha encontrado resultado de busqueda para:  <span> ${datoAb}</span>`
+        }
+        else{
+            mensaje.innerHTML = `Resultado de busqueda para:  <span>${datoAb}</span>`
+            for(let i=0; i<2; i++){    //Mostramos 2 pelis
+                seccionBuscador.innerHTML += `<article class="popu">
+                    <a href="./detalles-peliculas.html?id=${data.results[i].id}"><img class="pelis" 
